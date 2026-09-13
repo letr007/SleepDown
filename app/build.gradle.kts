26cd6adcd17b49db66ff50a3925ab907db848e9c
letr <letr007@foxmail.com>
2026-09-13 17:36:58 +0800

    chore(release): prepare 0.1.0-beta.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.letr.sleepdown"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.1.0-beta.3"
+        versionCode = 5
+        versionName = "0.1.0-beta.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
